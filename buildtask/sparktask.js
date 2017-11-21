0b8d58346981ff60f7a122f4b67e0a97757a7b71
Carlo Wahlstedt <carlowahlstedt@gmail.com>
2017-11-21 11:58:45 -0500

    Added additional logging.

--- buildtask/sparktask.js
+++ buildtask/sparktask.js
@@ -12,13 +12,18 @@ const tl = require("vsts-task-lib");
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
+            console.log('Getting the access token');
             process.env.CISCOSPARK_ACCESS_TOKEN = tl.getInput('access_token', true);
+            console.log('Getting the inputs');
             var teamName = tl.getInput('teamName', true).toUpperCase();
             var roomName = tl.getInput('roomName', true).toUpperCase();
             var message = tl.getInput('message', true);
             var team;
+            console.log('Preparing for a connection to Cisco Spark');
             const spark = require(`ciscospark/env`);
+            console.log('Getting the teams list');
             var teams = yield spark.teams.list();
+            console.log('Looking for a match');
             for (var index = 0; index < teams.length; index++) {
                 if (teams.items[index].name.toUpperCase() == teamName) {
                     console.log('Team found');
@@ -27,7 +32,9 @@ function run() {
                 }
             }
             if (team != null) {
+                console.log('Getting the rooms list');
                 var rooms = yield spark.rooms.list();
+                console.log('Looking for a match');
                 for (var index = 0; index < rooms.length; index++) {
                     var room = rooms.items[index];
                     if (room.teamId == team.id && room.title.toUpperCase() == roomName) {
@@ -47,7 +54,7 @@ function run() {
             tl.setResult(tl.TaskResult.Failed, "No Room Match Found for that Team");
         }
         catch (err) {
-            tl.setResult(tl.TaskResult.Failed, err.constructor.name + ": " + err.message);
+            tl.setResult(tl.TaskResult.Failed, err.message);
         }
     });
 }
--- buildtask/sparktask.ts
+++ buildtask/sparktask.ts
@@ -2,16 +2,21 @@ import tl = require('vsts-task-lib');
 
 async function run() {
     try {
+        console.log('Getting the access token');
         process.env.CISCOSPARK_ACCESS_TOKEN = tl.getInput('access_token', true);
 
+        console.log('Getting the inputs');
         var teamName = tl.getInput('teamName', true).toUpperCase();
         var roomName = tl.getInput('roomName', true).toUpperCase();
         var message = tl.getInput('message', true);
         var team: any;
 
+        console.log('Preparing for a connection to Cisco Spark');
         const spark = require(`ciscospark/env`);
 
+        console.log('Getting the teams list');
         var teams = await spark.teams.list();
+        console.log('Looking for a match');
         for (var index = 0; index < teams.length; index++) {
             if (teams.items[index].name.toUpperCase() == teamName) {
                 console.log('Team found');
@@ -21,7 +26,9 @@ async function run() {
         }
 
         if (team != null) {
+            console.log('Getting the rooms list');
             var rooms = await spark.rooms.list()
+            console.log('Looking for a match');
             for (var index = 0; index < rooms.length; index++) {
                 var room = rooms.items[index];
                 if (room.teamId == team.id && room.title.toUpperCase() == roomName) {
@@ -41,7 +48,7 @@ async function run() {
         tl.setResult(tl.TaskResult.Failed, "No Room Match Found for that Team");
     }
     catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.constructor.name + ": " + err.message);
+        tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
 
